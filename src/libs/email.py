9c0c473a7d6061220a85359820630a27d4315d6b
Roy Firestein <roy@autohost.ai>
2024-08-19 08:51:26 -0400

    Send HTML emails using Markdown parser

--- src/libs/email.py
+++ src/libs/email.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import markdown
 
 
 def send_email(to, subject, message):
@@ -11,11 +12,17 @@ def send_email(to, subject, message):
     :param message: Message of the email (plain text)
     :return: String "sent"
     """
+    html_body = markdown.markdown(message)
     data = {
         "from": f"Meeting Notes <no-reply@{os.environ.get('MAILGUN_DOMAIN', '')}>",
         "to": to,
         "subject": subject,
         "text": message,
+        "html": "".join([
+            "<html><body>",
+            html_body,
+            "</body></html>"
+        ])
     }
     result = requests.post(
         f"https://api.mailgun.net/v3/{os.environ.get('MAILGUN_DOMAIN', '')}/messages",
